c9b1d3f1e4e87df3358fabade9ef4954bce65463
Ferdinand Prantl <prantlf@gmail.com>
2020-02-15 06:23:47 +0100

    chore: Increase test code coverage

--- test/checker.js
+++ test/checker.js
@@ -56,11 +56,41 @@ addTest('passes if all tests does reach the minimum hz', assert =>
 addTest('fails if a test does not reach the minimum hz', assert =>
   checkFailure(assert, { hz: 20000000 }))
 
+addTest('passes if no test exceeds the maximum mean', assert =>
+  checkSuccess(assert, { mean: 1 }))
+
+addTest('fails if a test exceeds the maximum mean', assert =>
+  checkFailure(assert, { mean: 1e-9 }))
+
 addTest('passes if no test exceeds the maximum rme', assert =>
   checkSuccess(assert, { rme: 3 }))
 
 addTest('fails if a test exceeds the maximum rme', assert =>
   checkFailure(assert, { rme: 2 }))
+
+addTest('passes if no test exceeds the maximum moe', assert =>
+  checkSuccess(assert, { moe: 2e-9 }))
+
+addTest('fails if a test exceeds the maximum moe', assert =>
+  checkFailure(assert, { moe: 1.5e-9 }))
+
+addTest('passes if no test exceeds the maximum sem', assert =>
+  checkSuccess(assert, { sem: 2e-9 }))
+
+addTest('fails if a test exceeds the maximum sem', assert =>
+  checkFailure(assert, { sem: 1e-9 }))
+
+addTest('passes if no test exceeds the maximum deviation', assert =>
+  checkSuccess(assert, { deviation: 8e-9 }))
+
+addTest('fails if a test exceeds the maximum deviation', assert =>
+  checkFailure(assert, { deviation: 4e-9 }))
+
+addTest('passes if no test exceeds the maximum variance', assert =>
+  checkSuccess(assert, { variance: 6e-17 }))
+
+addTest('fails if a test exceeds the maximum variance', assert =>
+  checkFailure(assert, { variance: 2e-17 }))
 
 if (require.main === module) {
   require('test')
